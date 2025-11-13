5407f130b90a32e2706de172bc0cca93aa8afd7b
Martina <martina.ivanovska2004@gmail.com>
2025-11-13 10:32:23 +0100

    vtor lab search

--- src/main/java/mk/ukim/finki/wp/lab/repository/InMemoryBookRepository.java
+++ src/main/java/mk/ukim/finki/wp/lab/repository/InMemoryBookRepository.java
@@ -17,7 +17,7 @@ public class InMemoryBookRepository implements BookRepository {
     @Override
     public List<Book> searchBooks(String text, Double rating) {
         return DataHolder.books.stream()
-                .filter(b -> b.getTitle().equals(text))
+                .filter(b -> b.getTitle().contains(text))
                 .filter(b -> b.getAverageRating() >= rating)
                 .toList();
     }
